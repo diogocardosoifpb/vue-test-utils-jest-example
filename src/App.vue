1cdf14161355f98676791e57ce08e9cbe7420c15
Diogo Cardoso <diogomescardoso@gmail.com>
2023-10-19 20:21:55 -0300

    remover importação

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <rating :maxStars="5"/>
-    <meme />
   </div>
 </template>
 
 <script>
 import Rating from './components/Rating.vue'
-import Meme from './components/Random.vue'
 
 export default {
   components: {
-    Rating,
-    Meme
+    Rating
   }
 }
 </script>
