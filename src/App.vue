32a47e909c13c76766c7cb0df8556ad7d2c1fdbd
Diogo Cardoso <diogomescardoso@gmail.com>
2023-10-19 17:26:29 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    Rating
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+</style>
---
+++ test/components/Rating.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import Rating from '@/components/Rating.vue'
+
+let wrapper = null
+
+beforeEach(() => {
+  wrapper = shallowMount(Rating)
+})
+
+describe('Rating.vue', () => {
+})
